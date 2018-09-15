6e0dd4867aed78255d6ea254e0f3c9a524bf7124
Lionel CLEMENT <lclement@orange.fr>
2018-09-15 21:55:15 +0200

    Mise en ligne de l'exercice cours html + js

---
+++ Openclassrooms/Chapitre-1/js/cours.js
@@ -0,0 +1,9 @@
+console.log("Bonjour en JavaScript !");
+//console.log("Faisons quelques calculs.");
+console.log(4 + 7);
+//console.log(12 / 0);
+console.log("Au revoir !");
+
+console.log ("bonjour en JavaScript !");
+console.log (4 + 7);
+console.log ("Au revoir !");
